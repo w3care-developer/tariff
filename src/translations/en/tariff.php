7767ace9428186eed4fbc4903c360ffdd9b7b138
W3care Developer <testbyuser27@gmail.com>
2018-04-15 07:37:48 +0530

    init

---
+++ src/translations/en/tariff.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * tariff plugin for Craft CMS 3.x
+ *
+ * tariff
+ *
+ * @link      https://w3care.com
+ * @copyright Copyright (c) 2018 w3caredev
+ */
+
+/**
+ * tariff en Translation
+ *
+ * Returns an array with the string to be translated (as passed to `Craft::t('tariff', '...')`) as
+ * the key, and the translation as the value.
+ *
+ * http://www.yiiframework.com/doc-2.0/guide-tutorial-i18n.html
+ *
+ * @author    w3caredev
+ * @package   Tariff
+ * @since     0.0.1
+ */
+return [
+    'tariff plugin loaded' => 'tariff plugin loaded',
+];
